e5809c93ac45250927df8164a511366d2c01b76a
Bryon Bending <bryon.bending@suez.com>
2025-09-19 14:24:08 -0600

    Update to work on Task Scheduler

--- Backups/BackupMySQL80_ALLDBXdrive.cmd
+++ Backups/BackupMySQL80_ALLDBXdrive.cmd
@@ -8,6 +8,6 @@ SET logFileName=backup-MySQL80-ALLDB-X-drive-%dateTime%.log
 ECHO Starting Backup for MySQL80-ALLDB to X-drive Operations\MFE_SOFTWARE\MySQL80Backup folder %dateTime%
 
 REM robocopy "Z:\Operations" "X:\Operations" /mir /mt /zb /r:3  /LOG:C:\temp\backup\backup-X-Drive-2025-04-25.log
-"C:\Windows\System32\robocopy" "F:\MySQL80Backup" "X:\Operations\MFE_SOFTWARE\MySQL80Backup" /mir /mt:96 /zb /r:3 /LOG:C:\temp\backup\%logFileName%
+"C:\Windows\System32\robocopy" \\amusboufs01\F\MySQL80Backup \\fsx-am.wtswinlogon.com\Boulder\Operations\MFE_SOFTWARE\MySQL80Backup /mir /mt:96 /zb /r:3 /LOG:C:\temp\backup\%logFileName%
 	
 ECHO Exiting Backup for MySQL80-ALLDB to X-drive Operations\MFE_SOFTWARE\MySQL80Backup folder %dateTime% - BYE!
